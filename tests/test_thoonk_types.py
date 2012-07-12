a9ea5ca2c8648526afce9e9ad5501655b999600f
Iuri Diniz <iuridiniz@gmail.com>
2012-07-12 10:55:33 -0300

    Split tests into more classes

--- tests/test_thoonk_types.py
+++ tests/test_thoonk_types.py
@@ -6,37 +6,21 @@ Created on Jul 6, 2012
 from tests.test_thoonk_pubsub import TestThoonkBase
 from twisted.internet import defer
 
-class TestThoonkFeed(TestThoonkBase):
+
+class TestThoonkFeedBaseType(TestThoonkBase):
     @defer.inlineCallbacks
     def setUp(self):
         yield TestThoonkBase.setUp(self)
-
-        self.feed_name = "teste"
-        self.config = {'type': 'feed'}
+        self.feed_name = "test_feed_base_type"
+        self.config = {}
         yield self.pub.create_feed(self.feed_name, self.config)
 
-        from txthoonk.types import Feed
-        self.feed = Feed(pub=self.pub, name=self.feed_name)
+        from txthoonk.types.base import FeedBaseType
+        self.feed = FeedBaseType(pub=self.pub, name=self.feed_name)
 
         # check properties
         self.assertEqual(self.pub, self.feed.pub)
         self.assertEqual(self.feed_name, self.feed.name)
-
-        self.assertEqual(self.feed.feed_ids,
-                         "feed.ids:%s" % self.feed_name)
-        self.assertEqual(self.feed.feed_items,
-                         "feed.items:%s" % self.feed_name)
-        self.assertEqual(self.feed.feed_publishes,
-                         "feed.publishes:%s" % self.feed_name)
-        self.assertEqual(self.feed.feed_config,
-                         "feed.config:%s" % self.feed_name)
-
-        self.assertEqual(self.feed.channel_retract,
-                         "feed.retract:%s" % self.feed_name)
-        self.assertEqual(self.feed.channel_edit,
-                         "feed.edit:%s" % self.feed_name)
-        self.assertEqual(self.feed.channel_publish,
-                         "feed.publish:%s" % self.feed_name)
 
     ############################################################################
     #  Tests for config
@@ -55,6 +39,35 @@ class TestThoonkFeed(TestThoonkBase):
 
         ret = yield self.feed.get_config()
         self.assertEqual(ret, self.config)
+
+
+class TestThoonkFeed(TestThoonkBase):
+    @defer.inlineCallbacks
+    def setUp(self):
+        yield TestThoonkBase.setUp(self)
+
+        self.feed_name = "teste"
+        self.config = {'type': 'feed'}
+        yield self.pub.create_feed(self.feed_name, self.config)
+
+        from txthoonk.types import Feed
+        self.feed = Feed(pub=self.pub, name=self.feed_name)
+
+        self.assertEqual(self.feed.feed_ids,
+                         "feed.ids:%s" % self.feed_name)
+        self.assertEqual(self.feed.feed_items,
+                         "feed.items:%s" % self.feed_name)
+        self.assertEqual(self.feed.feed_publishes,
+                         "feed.publishes:%s" % self.feed_name)
+        self.assertEqual(self.feed.feed_config,
+                         "feed.config:%s" % self.feed_name)
+
+        self.assertEqual(self.feed.channel_retract,
+                         "feed.retract:%s" % self.feed_name)
+        self.assertEqual(self.feed.channel_edit,
+                         "feed.edit:%s" % self.feed_name)
+        self.assertEqual(self.feed.channel_publish,
+                         "feed.publish:%s" % self.feed_name)
 
     ############################################################################
     #  Tests for publish
@@ -310,10 +323,6 @@ class TestThoonkSortedFeed(TestThoonkBase):
 
         from txthoonk.types import SortedFeed
         self.feed = SortedFeed(pub=self.pub, name=self.feed_name)
-
-        # check properties
-        self.assertEqual(self.pub, self.feed.pub)
-        self.assertEqual(self.feed_name, self.feed.name)
 
         self.assertEqual(self.feed.feed_ids,
                          "feed.ids:%s" % self.feed_name)
